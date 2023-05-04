10b0de0733e47650bb952a80b9e5fee2d6eaa9d7
DoChuan13 <chuando.9213@gmail.com>
2023-05-05 00:03:43 +0700

    Servlet Dictionary

---
+++ src/main/java/rikkei/dictionary/TranslationServlet.java
@@ -0,0 +1,33 @@
+package rikkei.dictionary;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(value = "/translate") public class TranslationServlet extends HttpServlet {
+    @Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+        String search = request.getParameter("txtSearch");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if (result != null) {
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+        writer.println("</html>");
+    }
+}
